4acb2191427a1782df4668cd16cd18551de8c9ba
Beomjun Kim <nutyworks@gmail.com>
2020-06-18 20:57:30 +0900

    bob appears

--- dimibob.py
+++ dimibob.py
@@ -3,14 +3,12 @@ import urllib.request, json
 from colored import fg, bg, attr
 from datetime import datetime
 
-time = datetime(2020, 6, 17).isoformat().split('T')[0].replace('-','')
-print(time)
 max_day = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 bob_data = {}
+eng2kor = {'breakfast': '아침', 'lunch': '점심', 'dinner': '저녁'}
 
 def fetch_bob(month):
-    bob_data = {}
     with open('./bob.db', 'r') as f:
         for tmp in f.readlines():
             date = int(tmp.split('%')[0])
@@ -19,19 +17,19 @@ def fetch_bob(month):
                 'lunch': tmp.split('%')[2],
                 'dinner': tmp.split('%')[3].replace('\n', ''),
             }
-            print(date, meal)
+            # print(date, meal)
             bob_data[date] = meal
     for i in range(20200000 + month * 100 + 1, 20200000 + month * 100+ max_day[month] + 1):
         if i not in bob_data:
-            print("https://api.dimigo.in/dimibobs/" + str(i) + '/')
+            # print("https://api.dimigo.in/dimibobs/" + str(i) + '/')
             try:
                 with urllib.request.urlopen("https://api.dimigo.in/dimibobs/" + str(i) + '/') as url:
                     bob_data[i] = json.loads(url.read().decode())
-                    print("fetch %d:" % i, bob_data[i])
+                    # print("fetch %d:" % i, bob_data[i])
             except:
                 print('failed to fetch %d' % i)
                 break
-    print(bob_data)
+    # print(bob_data)
     with open('./bob.db', 'w') as f:
         for i in bob_data:
             f.write("%d%%%s%%%s%%%s\n" % (i, bob_data[i]['breakfast'], \
@@ -41,21 +39,30 @@ def fetch_bob(month):
 def main(stdscr):
     stdscr.clear()
     now = datetime.now()
-    now_str = "%d%02d%02d" % (now.year, now.month, now.day)
+    now_str = now.year * 10000 + now.month * 100 + now.day
     # keymap = # q: quit, l: next day, h: prev day    
     print(now_str)
-    eng2kor = {'breakfast': '아침', 'lunch': '점심', 'dinner': '저녁'}
-    with urllib.request.urlopen("http://api.dimigo.in/dimibobs/20200617/") as url:
-        data = json.loads(url.read().decode())
-        curses.init_pair(1, 184, 0)
-        curses.init_pair(2, 123, 0)
-        for k in eng2kor:
-            stdscr.addstr(eng2kor[k] + ' ', curses.color_pair(1))
-            for sp_meal in data[k].split('/'):
-                stdscr.addstr(sp_meal + '\n', curses.color_pair(2))
+    # print(bob_data[now_str])
+    # input()
+    curses.init_pair(1, 184, 0)
+    curses.init_pair(2, 123, 0)
+    line = 0
+    col = -24
+    for k in eng2kor:
+        line = 1
+        col += 25
+        stdscr.addstr(line, col, eng2kor[k] + ' ', curses.color_pair(1))
+        for j in bob_data[now_str][k].split('/'):
+            line += 1
+            if line < 40:
+                stdscr.addstr(line, col + 1, j + '\n')
+            # stdscr.addstr('\n')
+            # stdscr.addstr(j + '\n', curses.color_pair(2))
+        # stdscr.addstr(sp_meal + '\n', curses.color_pair(2))
         
     stdscr.refresh()
     stdscr.getkey()
 
-# curses.wrapper(main)
 fetch_bob(6)
+# print(bob_data)
+curses.wrapper(main)
